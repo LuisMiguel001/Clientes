abf6c6dcc8455f69170a5119c31afdfa7c53d45c
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-06-16 12:51:13 -0400

    Agregango al la carpeta Models la clase TicketsDetalle.

---
+++ Models/TicketsDetalle.cs
@@ -0,0 +1,13 @@
+﻿namespace Clientes.Models
+{
+    public class TicketsDetalle
+    {
+        public int Id { get; set; }
+
+        public int TicktetId { get; set; }
+
+        public string Emisor { get; set; }
+
+        public string Mensaje { get; set; }
+    }
+}
