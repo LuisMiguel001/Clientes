0d620b70c3b68b325fb044fa1305f8aae4561acc
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-06-17 14:57:43 -0400

    Agregando un Textarea al registro.

--- Models/TicketsDetalle.cs
+++ Models/TicketsDetalle.cs
@@ -10,10 +10,10 @@ namespace Clientes.Models
         [Required(ErrorMessage = "El campo (TicketId) es obligatorio")]
         public int TicktetId { get; set; }
 
-        [Required(ErrorMessage = "El campo (Emisos) es obligatorio")]
-        public string? Emisor { get; set; }
+        [Required(ErrorMessage = "El campo (Emisor) es obligatorio")]
+        public string Emisor { get; set; }
 
         [Required(ErrorMessage = "Debe dejar un detalle de mensaje")]
-        public string? Mensaje { get; set; }
+        public string Mensaje { get; set; }
     }
 }
