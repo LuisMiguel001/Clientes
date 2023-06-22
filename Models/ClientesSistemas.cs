f989b4f3f90ce34d2dfe191befecb101e8e51823
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-06-21 23:55:14 -0400

    Agregando el detalle al registro Clientes.

--- Models/ClientesSistemas.cs
+++ Models/ClientesSistemas.cs
@@ -12,6 +12,6 @@ namespace Clientes.Models
 
         public int SistemaId { get; set; }
 
-        public string? TerminalesPermitidas { get; set; }
+        public int TerminalesPermitidas { get; set; }
     }
 }
